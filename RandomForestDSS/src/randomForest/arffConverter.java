2e2c91033617b4ca84c46bac8862581f8bb1480f
leireva <leireviseras@gmail.com>
2016-03-14 18:55:32 +0100

    Aldaketak fitxategia irakurtzerakoan, test .csv fitxategia bada "?"

--- RandomForestDSS/src/randomForest/arffConverter.java
+++ RandomForestDSS/src/randomForest/arffConverter.java
@@ -16,9 +16,11 @@ public  void main(String path)throws Exception{
 		
 		//String helbidea;
 		String user = System.getProperty("user.name");
-		File f = new File("/home/"+user+"/train.arff");
+		
+		String fName = nameIt(path);
+		File f = new File("/home/"+user+"/"+fName+".arff");
 		System.out.println("====================");
-		System.out.println("Zure fitxategia hemen eratuko da : /home/"+user+"/train.arff");
+		System.out.println("Zure fitxategia hemen eratuko da : /home/"+user+"/"+fName+".arff");
 		if (!f.exists())
 			f.createNewFile();
 		else
@@ -32,7 +34,7 @@ public  void main(String path)throws Exception{
 		BufferedReader br = new BufferedReader(fr);
 		
 		//.arff fitxategi baten goiburua
-		bw.write("@RELATION tweetSentiment.dev.csv\n\n");
+		bw.write("@RELATION "+fName+".csv\n\n");
 		bw.write("@ATTRIBUTE Text string \n\n");
 		bw.write("@ATTRIBUTE CLASS {neutral,positive,negative}\n");
 		bw.write("@DATA\n");
@@ -46,7 +48,15 @@ public  void main(String path)throws Exception{
 					//String data;
 					/*data=atazak[3].substring(26, atazak[3].length())+" "+atazak[3].substring(11,19);*/
 					//bw.write(atazak[1]+",'"+atazak[4].replace("'", "�")+"'\n");
-					String myString = "'"+atazak[4].replace("'", "�")+"',"+atazak[1]+"\n";
+						String myString = "";
+						if(fName.equals("test")){
+							//test bada "?" agertuko da klasean
+							String unknown = "?";
+							myString = "'"+atazak[4].replace("'", "�")+"',"+unknown+"\n";
+						}else{
+							myString = "'"+atazak[4].replace("'", "�")+"',"+atazak[1]+"\n";
+						}
+					
 					byte ptext[] = myString.getBytes();
 					String value = new String(ptext, "UTF-8");
 					bw.write(value);
@@ -58,7 +68,17 @@ public  void main(String path)throws Exception{
 			br.close();
 		 }
 	
-
+	private String nameIt(String path){
+		String name = "" ;
+		if(path.contains("train")){
+			name = "train";
+		}else if(path.contains("dev")){
+			name = "dev";
+		} else if(path.contains("test")){
+			name = "test";
+		}
+		return name;
+	}
 	
 
 }
