f5ce98f5813bd4c5b99d7ac2a30ebafcdb96da2b
Bingen <zurrrakapote@gmail.com>
2016-04-06 21:02:08 +0200

    Dokumentazioa idatzita

--- RandomForestDSS/src/randomForest/Nagusia.java
+++ RandomForestDSS/src/randomForest/Nagusia.java
@@ -1,7 +1,10 @@
 package randomForest;
 
 public class Nagusia {
-
+	/*
+	*csv fitxategi baten path-a behar du.
+	*behin bat sartuta, arffconverter.main metodoa egingo du, hau da, cvs fitxategiko datuak arff fitxategi batean sartu.
+	*/
 	public static void main(String[] args) {
 		if( args.length < 1 ){
 			System.out.println("ARGUMENTOS:");
